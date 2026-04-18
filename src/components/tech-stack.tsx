84d615c3130768bc5b15b7772b8ccd1f74149c75
kat <143475611+puzzlesremixed@users.noreply.github.com>
2026-04-19 00:57:30 +0700

    fix: logo render on tech stacks

--- src/components/tech-stack.tsx
+++ src/components/tech-stack.tsx
@@ -1,4 +1,4 @@
-import {TECH_STACK_DATA, TechDetails, UNKNOWN_TECH,} from '@/lib/tech-stack';
+import {TECH_STACK_DATA, TechDetails, UNKNOWN_TECH} from "@/lib/tech-stack";
 import CustomSvg from "@/components/custom-svg";
 import {cn} from "@/lib/utils";
 
@@ -9,15 +9,17 @@ export const TechStack = ({stack}: { stack?: string[] }) => {
 
   return (
     <div className="">
-      <h3 className="text-lg font-mono text-muted-foreground border-b p-4 ">TECH STACK</h3>
+      <h3 className="text-lg font-mono text-muted-foreground border-b p-4 ">
+        TECH STACK
+      </h3>
       <div className="flex flex-col w-full">
         {stack.map((techName) => {
-          const techDetails = TECH_STACK_DATA[techName] || {...UNKNOWN_TECH, name: techName};
+          const techDetails = TECH_STACK_DATA[techName] || {
+            ...UNKNOWN_TECH,
+            name: techName,
+          };
           return (
-            <TechStackItem
-              key={techDetails.name}
-              techDetails={techDetails}
-            />
+            <TechStackItem key={techDetails.name} techDetails={techDetails}/>
           );
         })}
       </div>
@@ -29,8 +31,14 @@ const TechStackItem = ({techDetails}: { techDetails: TechDetails }) => {
   return (
     <div className="border-b flex flex-row">
       <div className="w-14 flex items-center justify-center py-3">
-        <CustomSvg src={techDetails.logo} className={cn("w-7 h-7 text-black dark:text-white", techDetails.className)}
-                   style={'filter: grayscale(60%) opacity(70%);'}/>
+        <CustomSvg
+          src={techDetails.logoWhite}
+          className={cn(
+            "w-7 h-7 text-black dark:text-white fill-current",
+            techDetails.className,
+          )}
+          style={""}
+        />
       </div>
       <div className={"p-4 border-l"}>{techDetails.name}</div>
     </div>
--- src/lib/tech-stack.ts
+++ src/lib/tech-stack.ts
@@ -1,60 +1,87 @@
 export type TechDetails = {
   name: string;
   logo?: string;
+  logoWhite?: string;
   className?: string;
+  url?:string;
 };
 
 export const TECH_STACK_DATA: Record<string, TechDetails> = {
   'nextjs': {
     name: 'Next.js',
     logo: '/logo/nextjs.svg',
-    className: 'fill-current',
+    logoWhite: '/logo/white/nextjs.svg',
+    className: '',
+    url: "https://nextjs.org/"
   },
   'typescript': {
     name: 'TypeScript',
-    logo: '/logo/typescript.svg'
+    logo: '/logo/typescript.svg',
+    logoWhite: '/logo/white/typescript.svg',
+    url: 'https://www.typescriptlang.org/'
   },
   'prisma': {
     name: 'Prisma',
     logo: '/logo/prismaorm.svg',
-    className: ' fill-current',
+    logoWhite: '/logo/white/prismaorm.svg',
+    className: '',
+    url: 'https://www.prisma.io/docs/orm'
   },
   'postgresql': {
     name: 'PostgreSQL',
-    logo: '/logo/postgresql.svg'
+    logo: '/logo/postgresql.svg',
+    logoWhite: '/logo/white/postgresql.svg',
+    url: 'https://www.postgresql.org/'
   },
   'tailwindcss': {
     name: 'Tailwind CSS',
-    logo: '/logo/tailwindcss.svg'
+    logo: '/logo/tailwindcss.svg',
+    logoWhite: '/logo/white/tailwindcss.svg',
+    url: 'https://tailwindcss.com/'
   },
   'reactjs': {
     name: 'React',
-    logo: '/logo/reactjs.svg'
+    logo: '/logo/reactjs.svg',
+    logoWhite: '/logo/white/reactjs.svg',
+    url: 'https://react.dev/'
   },
   'laravel': {
     name: 'Laravel',
-    logo: '/logo/laravel.svg'
+    logo: '/logo/laravel.svg',
+    logoWhite: '/logo/white/laravel.svg',
+    className: '',
+    url: 'https://laravel.com/'
   },
   'javascript': {
     name: 'JavaScript',
-    logo: '/logo/javascript.svg'
+    logo: '/logo/javascript.svg',
+    logoWhite: '/logo/white/javascript.svg',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
   },
   'inertiajs': {
     name: 'Inertia',
-    logo: '/logo/inertiajs.svg'
+    logo: '/logo/inertiajs.svg',
+    logoWhite: '/logo/white/inertiajs.svg',
+    url: 'https://inertiajs.com/'
   },
   'payloadcms': {
     name: 'Payload',
     logo: '/logo/payloadcms.svg',
-    className: 'invert',
+    logoWhite: '/logo/white/payloadcms.svg',
+    className: '',
+    url: 'https://payloadcms.com/'
   },
   'statamic': {
     name: 'Statamic',
-    logo: '/logo/statamic.svg'
+    logo: '/logo/statamic.svg',
+    logoWhite: '/logo/white/statamic.svg',
+    url: 'https://statamic.com'
   },
-  'php':{
-    name:'PHP',
-    logo: '/logo/php.svg'
+  'php': {
+    name: 'PHP',
+    logo: '/logo/php.svg',
+    logoWhite: '/logo/white/php.svg',
+    url: 'https://www.php.net/'
   }
 };
 
